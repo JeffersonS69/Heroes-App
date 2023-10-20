1527adc1efd3413caeae569d4f166127c768730b
Jefferson <ibarra.jefferson1@gmail.com>
2023-10-20 17:24:15 -0500

    Router login and logout

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export const Login = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogin = () => {
+    login('Jefferson Garcia')
     navigate("/", {
       replace: true,
     });
